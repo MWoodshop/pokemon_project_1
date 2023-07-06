42153749302ad24cdd6cea9b0595ae6390ab9eff
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-06 19:26:31 -0400

    feat: updates HTML on several pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/', to: 'welcome#index'
+  get '/', to: 'welcome#index', as: 'welcome'
 
-  get '/pokemons', to: 'pokemons#index'
-  get '/pokemons/new', to: 'pokemons#new'
-  post '/pokemons', to: 'pokemons#create'
-  get '/pokemons/:id', to: 'pokemons#show'
-  get '/pokemons/:id/edit', to: 'pokemons#edit'
-  patch '/pokemons/:id', to: 'pokemons#update'
-  delete '/pokemons/:id', to: 'pokemons#destroy'
+  get '/pokemons', to: 'pokemons#index', as: 'pokemons'
+  get '/pokemons/new', to: 'pokemons#new', as: 'new_pokemon'
+  post '/pokemons', to: 'pokemons#create', as: 'create_pokemon'
+  get '/pokemons/:id', to: 'pokemons#show', as: 'pokemon'
+  get '/pokemons/:id/edit', to: 'pokemons#edit', as: 'edit_pokemon'
+  patch '/pokemons/:id', to: 'pokemons#update', as: 'update_pokemon'
+  delete '/pokemons/:id', to: 'pokemons#destroy', as: 'destroy_pokemon'
 end
