959a12a123d0890f16d0b80e3e1f3333eef6a288
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-10 20:46:26 -0400

    test: adds validation tests

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe Pokemon, type: :model do
                            trainer_id: 1)
   end
   it 'exists' do
-    pokemon = Pokemon.new
-    expect(pokemon).to be_a Pokemon
+    expect(@pokemon).to be_a Pokemon
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :pokemon_type }
+    it { should validate_presence_of :height }
+    it { should validate_presence_of :weight }
+    it { should validate_presence_of :trainer_id }
+    it { should validate_inclusion_of(:can_evolve).in_array([true, false]) }
   end
 end
--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
   before(:each) do
-    @trainer = Trainer.new(name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer = Trainer.new(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
   end
   it 'exists' do
-    trainer = Trainer.new
-    expect(trainer).to be_a Trainer
+    expect(@trainer).to be_a Trainer
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :region }
+    it { should validate_presence_of :generation_introduced }
+    it { should validate_inclusion_of(:gym_leader).in_array([true, false]) }
   end
 end
