298f15513f55e481463853830fdbfff51438a3b7
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-11 19:39:54 -0400

    Feat: Adds FactoryBot testing config to model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,8 +3,11 @@ class Pokemon < ApplicationRecord
 
   validates :name, presence: true
   validates :pokemon_type, presence: true
-  validates :trainer, presence: true
+  validates :trainer_id, presence: true
   validates :height, presence: true
   validates :weight, presence: true
   validates :can_evolve, inclusion: { in: [true, false] }
+
+  scope :true_pokemon, -> { where(can_evolve: true) }
+  scope :false_pokemon, -> { where(can_evolve: false) }
 end
