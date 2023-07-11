ade2ae66842f94dabc15b3da6c6b4cddd31904f1
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-10 21:19:59 -0400

    Refactor: Seperates out model and feature testing

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Pokemon, type: :model do
   before(:each) do
+    @trainer = Trainer.new(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1,
+                           gym_leader: false)
     @pokemon = Pokemon.new(name: 'Pikachu', pokemon_type: 'Electric', height: 1, weight: 13, can_evolve: true,
                            trainer_id: 1)
   end
@@ -14,7 +16,32 @@ RSpec.describe Pokemon, type: :model do
     it { should validate_presence_of :pokemon_type }
     it { should validate_presence_of :height }
     it { should validate_presence_of :weight }
-    it { should validate_presence_of :trainer_id }
-    it { should validate_inclusion_of(:can_evolve).in_array([true, false]) }
+  end
+
+  # Tests the foreign key constraint
+  describe 'associations' do
+    it { should belong_to :trainer }
+  end
+end
+
+feature 'Pokemon Index Page', type: :feature do
+  before(:each) do
+    @trainer1 = Trainer.create(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer2 = Trainer.create(id: 2, name: 'Misty', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer3 = Trainer.create(id: 3, name: 'Brock', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+
+    @pokemon1 = Pokemon.create(name: 'Pikachu', pokemon_type: 'Electric', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer1.id)
+    @pokemon2 = Pokemon.create(name: 'Squirtle', pokemon_type: 'Water', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer2.id)
+    @pokemon3 = Pokemon.create(name: 'Charmander', pokemon_type: 'Fire', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer3.id)
+  end
+
+  scenario 'displays all trainers on the /trainers page' do
+    visit pokemons_path
+    expect(page).to have_content(@pokemon1.name)
+    expect(page).to have_content(@trainer2.name)
+    expect(page).to have_content(@trainer3.name)
   end
 end
--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -2,16 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
   before(:each) do
-    @trainer = Trainer.new(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer1 = Trainer.new(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer2 = Trainer.new(id: 2, name: 'Misty', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer3 = Trainer.new(id: 3, name: 'Brock', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+
+    @pokemon1 = Pokemon.new(name: 'Pikachu', pokemon_type: 'Electric', height: 1, weight: 13, can_evolve: true,
+                            trainer_id: @trainer1.id)
+    @pokemon2 = Pokemon.new(name: 'Squirtle', pokemon_type: 'Water', height: 1, weight: 13, can_evolve: true,
+                            trainer_id: @trainer2.id)
+    @pokemon3 = Pokemon.new(name: 'Charmander', pokemon_type: 'Fire', height: 1, weight: 13, can_evolve: true,
+                            trainer_id: @trainer3.id)
   end
   it 'exists' do
-    expect(@trainer).to be_a Trainer
+    expect(@trainer1).to be_a Trainer
   end
 
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :region }
     it { should validate_presence_of :generation_introduced }
-    it { should validate_inclusion_of(:gym_leader).in_array([true, false]) }
+  end
+end
+
+feature 'Trainer Index Page', type: :feature do
+  before(:each) do
+    @trainer1 = Trainer.create(id: 1, name: 'Ash', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer2 = Trainer.create(id: 2, name: 'Misty', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+    @trainer3 = Trainer.create(id: 3, name: 'Brock', region: 'Kanto', generation_introduced: 1, gym_leader: false)
+
+    @pokemon1 = Pokemon.create(name: 'Pikachu', pokemon_type: 'Electric', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer1.id)
+    @pokemon2 = Pokemon.create(name: 'Squirtle', pokemon_type: 'Water', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer2.id)
+    @pokemon3 = Pokemon.create(name: 'Charmander', pokemon_type: 'Fire', height: 1, weight: 13, can_evolve: true,
+                               trainer_id: @trainer3.id)
+  end
+
+  # User Story 1
+  scenario 'displays all trainers on the /trainers page' do
+    visit trainers_path
+    expect(page).to have_content(@trainer1.name)
+    expect(page).to have_content(@trainer2.name)
+    expect(page).to have_content(@trainer3.name)
   end
 end
